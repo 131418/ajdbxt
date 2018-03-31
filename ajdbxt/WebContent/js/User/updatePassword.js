80a3f3011b77f09c5ed75ec98e03a7294659f49c
lyq <lyq@DESKTOP-7POSPRQ.lan>
2018-03-31 20:48:04 +0800

    updatePassword...

---
+++ ajdbxt/WebContent/js/User/updatePassword.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+var xhr;
+function updatePassword() {
+	var newPassword = document.getElementById("newPassword").value;
+	var newPasswordAgain = document.getElementById("newPasswordAgain").value;
+	if (newPassword == "" || newPassword == null) {
+		toastr.error("新密码不能为空！");
+	} else if (newPasswordAgain == "" || newPasswordAgain == null) {
+		toastr.error("确认密码不能为空！");
+	} else if (newPassword != newPasswordAgain) {
+		toastr.error("两次密码不一致！");
+	} else {
+		getXmlHttp();
+		xhr.open("POST", "/ajdbxt/user/User_changePassword", true);
+		var formData = new FormData();
+		formData.append("ajdbxt_police.policePassword", newPassword);
+		xhr.send(formData);
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && xhr.status == 200) {
+				var result = xhr.responseText;
+				if (result == "success") {
+					toastr.success("修改成功！");
+					window.location = "/ajdbxt/user/User_login";
+				} else {
+					toastr.error("修改失败！");
+				}
+			}
+		}
+	}
+}
+
+function getXmlHttp() {
+	if (window.XMLHttpRequest) {
+		// IE7+, Firefox, Chrome, Opera, Safari 浏览器执行代码
+		xhr = new XMLHttpRequest();
+	} else {
+		// IE6, IE5 浏览器执行代码
+		xhr = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+}
