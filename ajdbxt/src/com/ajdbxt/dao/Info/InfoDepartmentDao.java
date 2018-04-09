ad9d0db7019cd361a1c12f3239f7faca42f7aa91
LiWenKaigit <18779573765@163.com>
2018-04-09 14:37:17 +0800

    a

---
+++ ajdbxt/src/com/ajdbxt/dao/Info/InfoDepartmentDao.java
@@ -0,0 +1,9 @@
+package com.ajdbxt.dao.Info;
+
+import java.util.List;
+
+import com.ajdbxt.domain.DO.ajdbxt_department;
+
+public interface InfoDepartmentDao {
+	public List<ajdbxt_department> findAllDepartment();
+}
--- ajdbxt/src/com/ajdbxt/domain/VO/Info/LegalSystemAndLeadersVO.java
+++ ajdbxt/src/com/ajdbxt/domain/VO/Info/LegalSystemAndLeadersVO.java
@@ -2,11 +2,19 @@ package com.ajdbxt.domain.VO.Info;
 
 import java.util.List;
 
+import com.ajdbxt.domain.DO.ajdbxt_department;
 import com.ajdbxt.domain.DO.ajdbxt_police;
 
 public class LegalSystemAndLeadersVO {
 	private List<ajdbxt_police> legals;
 	private List<ajdbxt_police> leaders;
+	private List<ajdbxt_department> departments;
+	public List<ajdbxt_department> getDepartments() {
+		return departments;
+	}
+	public void setDepartments(List<ajdbxt_department> departments) {
+		this.departments = departments;
+	}
 	public List<ajdbxt_police> getLeaders() {
 		return leaders;
 	}
