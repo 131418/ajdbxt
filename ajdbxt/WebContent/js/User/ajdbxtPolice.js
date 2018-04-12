dc86408a4312c9fdbf85ccda90f084edeabf530e
Xbetty <1623290520@qq.com>
2018-04-12 17:58:10 +0800

    111

--- ajdbxt/WebContent/js/User/ajdbxtPolice.js
+++ ajdbxt/WebContent/js/User/ajdbxtPolice.js
@@ -331,7 +331,7 @@ function createPolice() {
 										for (var len = 0; len < Department_data.list.length; len++) {
 											option += '<option ';
 											option += ' value="'
-													+ Department_data.list[len].department_name
+													+ Department_data.list[len].ajdbxt_department_id
 													+ '">'
 													+ Department_data.list[len].department_name
 													+ '</option>';
@@ -570,7 +570,7 @@ function updatePolice(button) {
 														option += 'selected';
 													}
 													option += ' value="'
-															+ Department_data.list[len].department_name
+															+ Department_data.list[len].ajdbxt_department_id
 															+ '">'
 															+ Department_data.list[len].department_name
 															+ '</option>';
