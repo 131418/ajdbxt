eca50af4a567d8ceb3f97b64db3ffd9362bd4569
gxr <1074545235@qq.com>
2018-05-06 21:52:14 +0800

    5.6 21:52修改

--- ajdbxt/src/com/ajdbxt/dao/Info/InfoPoliceDao.java
+++ ajdbxt/src/com/ajdbxt/dao/Info/InfoPoliceDao.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.ajdbxt.dao.Info;
 
 import java.util.List;
@@ -13,21 +12,3 @@ public interface InfoPoliceDao {
 	public ajdbxt_police findCaptainByDepartment(String department_id);
 	public List<ajdbxt_police> findLegalOfDepartment();
 }
-
-=======
-package com.ajdbxt.dao.Info;
-
-import java.util.List;
-
-import com.ajdbxt.domain.DO.ajdbxt_police;
-
-public interface InfoPoliceDao {
-	public List<ajdbxt_police> findPoliceByDepartment(String department);
-	public ajdbxt_police findPoliceById(String police_id);
-	public List<ajdbxt_police> findLegals();
-	public List<ajdbxt_police> findLeaders();
-	public ajdbxt_police findCaptainByDepartment(String department_id);
-	public List<ajdbxt_police> findLegalOfDepartment();
-}
-
->>>>>>> LWK
